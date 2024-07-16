f0145b1b681b4823cc56e6e71be4a0412b7ded15
fedenemi <39474959+fedenemi@users.noreply.github.com>
2024-07-15 22:31:39 -0300

    Update parcial.py

--- parcial.py
+++ parcial.py
@@ -6,7 +6,7 @@ gens_f =  franquicia.flatmap(lambda x: [(x[0], w) for w in x[4].split()])
 gens_j = games.flatmap(lambda x: [(x[2], w) for w in x[-1].split()])
 joined = gens_f.join(gens_j).map(lambda x: ((x[1][0], x[1][1]), 1))
 gens_cant = joined.reduceByKey(lambda x, y: x + y).map(lambda x: (x[0][0], (x[0][1], x[1])))
-gens_cant.reduce(lambda x, y: x if x[1] > y[1] else y)
+gens_cant.reduceByKey(lambda x, y: x if x[1] > y[1] else y)
 
 
     
